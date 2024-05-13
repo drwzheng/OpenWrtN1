648f72a051ccb622347bb43679aaa2989790d4c3
drwzheng <36002292+drwzheng@users.noreply.github.com>
2024-05-13 22:18:31 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -39,6 +39,12 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
+
+# 迅雷
+git clone https://github.com/sbwml/luci-app-xunlei package/xunlei
+make menuconfig # choose LUCI -> Applications -> luci-app-xunlei
+make V=s
+
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
