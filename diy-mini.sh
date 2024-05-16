a8f753f500e9bf5a97726ff0329d13db82dee71c
drwzheng <36002292+drwzheng@users.noreply.github.com>
2024-05-16 18:05:30 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -40,13 +40,6 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 # git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 
-# 迅雷
-# git clone https://github.com/gngpp/thunder && cd thunder
-# cargo build --release && mv target/release/thunder 
-git clone https://github.com/gngpp/xunlei && cd xunlei
-cargo build --release && mv target/release/xunlei .
-# bash +x ./unpack.sh && cargo build --release --features embed && mv target/release/xunlei .
-
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
